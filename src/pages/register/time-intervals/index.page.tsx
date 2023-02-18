141e2f66f9e287acc515d76507b32357dcb70007
Mujapira <malmasmello2@gmail.com>
2023-02-18 12:25:42 -0300

    Availability: Zod input and output & fix: Zod validation

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,9 +1,12 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Checkbox, Heading, MultiStep, Text } from '@mujapira-ui/react'
 import { ArrowRight, Clock } from 'phosphor-react'
-import { useForm, useFieldArray, Controller } from 'react-hook-form'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { convertTimeStringToMinutes } from '../../../utils/convert-time-string-to-minutes'
 import { getWeekDays } from '../../../utils/get-week-days'
 import { Container, Header } from '../styles'
+
 import {
   CustomTextInput,
   FormError,
@@ -14,35 +17,56 @@ import {
   IntervalItem,
 } from './styles'
 
-export default function TimeIntervals() {
-  const timeIntervalsFormSchema = z.object({
-    intervals: z
-      .array(
-        z.object({
-          weekDay: z.number().min(0).max(6),
-          enabled: z.boolean(),
-          startTime: z.string(),
-          endTime: z.string(),
-        }),
-      )
-      .length(7)
-      .transform((intervals) =>
-        intervals.filter((interval) => interval.enabled),
-      )
-      .refine((intervals) => intervals.length > 0, {
-        message: 'Você precisa selecionar pelo menos um dia da semana',
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
       }),
-  })
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana',
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término deve ser pelo menos 1h distante do início.',
+      },
+    ),
+})
 
-  type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
+export default function TimeIntervals() {
   const {
     register,
     handleSubmit,
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -62,10 +86,13 @@ export default function TimeIntervals() {
     control,
     name: 'intervals',
   })
+
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data)
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+
+    console.log(formData)
   }
 
   return (
